e7e431bef9b17b90dbf0f2f873a11d0d307c473d
Oli Mcc <dev@olimcc.com>
2012-04-17 01:25:41 -0700

    include live/dev urls in app.js. remove console logs

--- presentation/js/app.js
+++ presentation/js/app.js
@@ -1,24 +1,26 @@
+var DEV_ENDPOINT = 'http://localhost:8000/stubs/foo.json';
+var LIVE_ENDPOINT = 'http://localhost:8080/api';
+
 $(function() {
 
   var source = "Here are some interesting places:<br>" +
                "<ul class='foo'>" +
                "{{#each places}}" +
-               "<li>{{this.county}}, {{this.country}} with population {{this.count}}</li>" +
+               "<li>{{this.town}}, {{this.country}} with population {{this.count}}</li>" +
                "{{/each}}" +
                "</ul>";
 
   var template = Handlebars.compile(source);
 
   $.ajax({
-    url: 'http://localhost:8000/stubs/foo.json',
+    url: LIVE_ENDPOINT,
     dataType: 'json',
     success: function(resp) {
-      console.log(resp);
       var result = template(resp);
       $('#demo').html(result);
     },
     error: function(e, x) {
-      console.log(e, x)
+      console.log(e, x);
     }
   });
 
