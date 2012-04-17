97b3c90cc418ec4b9d9bec388454d62be6a54b49
Oli Mcc <dev@olimcc.com>
2012-04-17 01:22:07 -0700

    add serving of simple api and static html content to demo-py

--- demo-py/app.py
+++ demo-py/app.py
@@ -1,7 +1,20 @@
 from bottle import route, run, static_file
+import json
 
 @route('/demo')
 def index():
     return static_file('demo.html', root='/Users/oli/src/preso/presentation/')
+
+@route('/js/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='/Users/oli/src/preso/presentation/js')
+
+@route('/api')
+def api():
+  data = {'places': []}
+  for e in ('SF', 'Mountain View', 'Napa'):
+    someResult = {'town': e, 'county': 'SF', 'country': 'USA', 'count': 100000}
+    data['places'].append(someResult)
+  return json.dumps(data)
 
 run(host='localhost', port=8080)
